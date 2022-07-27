b6c559b2440b27627e2d775a0cde060894897277
Shuhao Wu <shuhao@shuhaowu.com>
2022-07-27 18:02:32 -0400

    Initial build system import

---
+++ cmake/FindDAQNavi.cmake
@@ -0,0 +1,26 @@
+include(FindPackageHandleStandardArgs)
+
+find_library(DAQNAVI_LIBRARY
+  NAMES
+  biodaq
+)
+
+find_path(DAQNAVI_INCLUDE_DIR
+  NAMES
+  inc/bdaqctrl.h
+  PATHS
+  /opt/advantech
+)
+
+find_package_handle_standard_args(biodaq REQUIRED_VARS DAQNAVI_LIBRARY DAQNAVI_INCLUDE_DIR)
+
+if (biodaq_FOUND)
+  mark_as_advanced(DAQNAVI_LIBRARY)
+  mark_as_advanced(DAQNAVI_INCLUDE_DIR)
+endif()
+
+if (biodaq_FOUND AND NOT TARGET biodaq)
+  add_library(biodaq SHARED IMPORTED)
+  set_property(TARGET biodaq PROPERTY IMPORTED_LOCATION ${DAQNAVI_LIBRARY})
+  target_include_directories(biodaq INTERFACE ${DAQNAVI_INCLUDE_DIR})
+endif()
