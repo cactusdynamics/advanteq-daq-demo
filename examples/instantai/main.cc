3f270121c4c0c6ec2202933e3da54de376c9f1ef
Shuhao Wu <shuhao@shuhaowu.com>
2022-08-19 12:11:57 -0400

    Updated

--- examples/instantai/main.cc
+++ examples/instantai/main.cc
@@ -24,7 +24,7 @@ int main(int argc, const char* argv[]) {
   spdlog::set_level(spdlog::level::debug);
 #endif
 
-  if (argc <= 1) {
+  if (argc <= 2) {
     SPDLOG_ERROR("error: must specify device description in the format of USB-4704,BID#0 as the second argument");
     return 1;
   }
@@ -32,7 +32,10 @@ int main(int argc, const char* argv[]) {
   auto     n = strlen(argv[1]);
   wchar_t* device_desc = new wchar_t[n + 1];
   std::mbstowcs(device_desc, argv[1], n);
-  SPDLOG_DEBUG("Using device {}", argv[1]);
+
+  bool differential = std::string(argv[2]) == "differential";
+
+  SPDLOG_DEBUG("Using device {} (differential: {})", argv[1], differential);
 
   // Create an AI control object in "instant mode"
   InstantAiCtrl*    instant_ai_ctrl = InstantAiCtrl::Create();
@@ -49,23 +52,6 @@ int main(int argc, const char* argv[]) {
   SPDLOG_INFO("The analog input of this device has a data size of {}.", data_size);
 
   Array<AiChannel>* channels = instant_ai_ctrl->getChannels();
-  // There's a hardware jumper on the USB-4704 that controls if the channels are
-  // in single-ended or differential mode. You cannot set a single pair of
-  // channels to be in differential mode.
-  // From the docs about AI Channel Types:
-  // > In general, the ChannelType property value of the selected device will be
-  // > one of the AiChannelType enumeration, but there are some special cases, for
-  // > example ChannelType property value of device USB-4702 and USB-4704 is
-  // > decided by the hardware jumper, it could be AllSingleEnded or
-  // > AllDifferential, if it is AllSingleEnded, all the channel can only be set
-  // > to single-ended and can't change to be differential by programming.  Please
-  // > note that the case of USB-4702 and USB-4704 is different from the
-  // > conditions that the ChannelType property value of the selected device
-  // > equals to AllSeDiffAdj, the latter means all the AI channels can be set to
-  // > single-ended at the same time and can change all the AI channels to be
-  // > differential by programming.
-  //
-  // However, according to the 4704 manual, there are no jumpers?
   SPDLOG_INFO("This device has {} analog channels:", channels->getCount());
   for (int i = 0; i < channels->getCount(); ++i) {
     auto& channel = channels->getItem(i);
@@ -77,57 +63,59 @@ int main(int argc, const char* argv[]) {
       channel.getSignalType());  // Value of 0 is Single ended.
   }
 
+  auto signal_type = differential ? Differential : SingleEnded;
+  auto value_range = V_Neg10To10;
+
+  // Setting the signal type and value range
   for (int i = 0; i < channels->getCount(); ++i) {
-    ret = channels->getItem(i).setSignalType(Differential);
+    ret = channels->getItem(i).setSignalType(signal_type);
+    if (BioFailed(ret)) {
+      PrintDaqError(ret);
+      exit(1);
+    }
+
+    ret = channels->getItem(i).setValueRange(value_range);
     if (BioFailed(ret)) {
       PrintDaqError(ret);
       exit(1);
     }
   }
-  // // Let's set channel 2 and 3 to differential:
-  // // This is not allowed on my USB-4704
-  // ret = channels->getItem(2).setSignalType(Differential);
-  // if (BioFailed(ret)) {
-  //   PrintDaqError(ret);
-  //   return 1;
-  // }
 
-  int16_t data_raw[2] = {0};
-  double  data_scaled[2] = {0};
+  // Verify the signal types are set
+  SPDLOG_INFO("After setting signal type, this device has {} analog channels:", channels->getCount());
 
-  while (true) {
-    ret = instant_ai_ctrl->Read(2, 2, data_raw, data_scaled);
+  channels = instant_ai_ctrl->getChannels();  // reget the channel, but there's memory leak problem as the previous channels is not cleaned up..
+  for (int i = 0; i < channels->getCount(); ++i) {
+    auto& channel = channels->getItem(i);
+    printf("- Channel %d -> value range: %d; signal type: %d\n", channel.getChannel(), channel.getValueRange(), channel.getSignalType());
+  }
+
+  int16_t data_raw[8] = {0};
+  double  data_scaled[8] = {0};
+
+  // Read data for 10 seconds
+  for (int i = 0; i < 50; ++i) {
+    ret = instant_ai_ctrl->Read(0, 8, data_raw, data_scaled);
     if (BioFailed(ret)) {
       PrintDaqError(ret);
       return 1;
     }
 
-    SPDLOG_INFO("Read data from channel 2 and 3: {} {} (raw), {} {} (scaled)", data_raw[0], data_raw[1], data_scaled[0], data_scaled[1]);
+    SPDLOG_INFO(
+      "Read data {:.2f} {:.2f} {:.2f} {:.2f} {:.2f} {:.2f} {:.2f} {:.2f} (scaled)",
+      data_scaled[0],
+      data_scaled[1],
+      data_scaled[2],
+      data_scaled[3],
+      data_scaled[4],
+      data_scaled[5],
+      data_scaled[6],
+      data_scaled[7]);
 
-    std::this_thread::sleep_for(300ms);
+    std::this_thread::sleep_for(200ms);
   }
 
-  // uint8_t state = 0;
-
-  // while (true) {
-  //   state = (state + 1) % 2;
-  //   uint8_t port_data = 0;
-
-  //   ret = instant_ai_ctrl->Read(0, port_data);
-  //   if (BioFailed(ret)) {
-  //     PrintDaqError(ret);
-  //     return 1;
-  //   }
-
-  //   std::bitset<8> port_bits(port_data);
-  //   SPDLOG_INFO("Read data: {} - {}", port_bits.to_string(), port_bits[0]);
-
-  //   std::this_thread::sleep_for(300ms);
-  // }
-
-  // Close and cleanup the device.
-  // TODO: wrap this with RAII.
   instant_ai_ctrl->Dispose();
-  delete device_desc;
+  delete[] device_desc;
   return 0;
 }
